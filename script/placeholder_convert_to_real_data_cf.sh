ca726a1aff79d436fc758f8d78e57268d363eb3a
jianglianEin <913057041@qq.com>
2020-02-27 19:19:12 +0800

    [JiShi] change cf username and password to placeholder.

--- script/placeholder_convert_to_real_data_cf.sh
+++ script/placeholder_convert_to_real_data_cf.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 sed -i "s#\${APP_ENV}#${APP_ENV}#" ./src/main/resources/application.yml
+sed -i "s#\${PASSWORD}#${PASSWORD}#" .travis.yml
+sed -i "s#\${USERNAME}#${USERNAME}#" .travis.yml
